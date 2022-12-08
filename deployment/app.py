6c862348b53285c3a05d3ae2b081b6de16e7a0f4
Lesmon Andres Lenin Lachica Saluta <lesmon_saluta@dlsu.edu.ph>
2022-12-08 23:20:30 +0800

    added visualization function to be called to show a graph

--- deployment/app.py
+++ deployment/app.py
@@ -1,24 +1,39 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from pages import Pages
-from flask import Flask
-from flask.templating import render_template
-
-def index():
-    return render_template('index.html', name='home')
-    
+from charts_functions_compiled import charts
 
 list_of_pages = [
     "Testing 1",
-    "Testing 2"
+    "Testing 2",
+    "Charts Dump"
 ]
 
-#st.sidebar.title('Breaking into the US! :notes:')
-#st.sidebar.markdown('by Group Mic :microphone: | DSFC10')
-selection = option_menu("Go to: ", list_of_pages, orientation='horizontal')
+st.write("test")
+
+# time_series_initial
+# local_vs_foreign
+# room_types
+# occupant_types
+# nights_stayed
+# time_series_month
+# time_series_year
+# wordcloud - doesn't print yet
+# quality_and_stars - doesn't print yet
+# facilities_positive
+# facilities_negative
+
+charts.facilities_positive() 
+
+# Left the inside black so that it really looks like a navbar
+selection = option_menu("", list_of_pages, orientation='horizontal')
 
 if selection == "Testing 1":
     Pages.page_one()
 
 elif selection == "Testing 2":
     Pages.page_two()
+    
+elif selection == "Charts Dump":
+    Pages.page_three()
+    
--- deployment/pages.py
+++ deployment/pages.py
@@ -1,7 +1,47 @@
-import pandas as pd
-import seaborn as sns
+import numpy as np 
+import pandas as pd 
 import streamlit as st
-import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime as dt
+import plotly as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+### ---------------------------------------
+import warnings
+warnings.filterwarnings('ignore')
+import pickle
+from _1_load_data import Load_Data
+from _2_visuals import Graphs
+from _2_chart_functions import Chart_Functions
+
+### ---------------------------------------
+dl=Load_Data()
+cf=Chart_Functions()
+g=Graphs()
+
+df_og=dl.pp_raw_hotel_data()
+### ---------------------------------------
+
+'''
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('display.float_format', '{:,.5f}'.format)
+'''
+
+
+pd.set_option('display.float_format', '{:,.2f}'.format)
 
 # Setting general format to the graphs
 sns.set_theme(style="white", font="sans-serif")
@@ -36,3 +76,9 @@ class Pages:
             
         subheader = '<p style="font-family:Arial; font-size: 40px; text-align: left;">Sample text <b>in HTML</b></p>'
         st.markdown(subheader, unsafe_allow_html=True)
+        
+    def page_three():
+        st.title(
+            "Charts Dump, Unformatted"
+        )
+        
